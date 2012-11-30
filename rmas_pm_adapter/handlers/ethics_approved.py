441bc0a5186b05a1cbf2b86b345c873620f01065
Jason Marshall <ctrlspc@gmail.com>
2012-11-30 10:51:18 +0000

    Added basic shell of an adapter including an empty
    handler for the ethics_approved event

---
+++ rmas_pm_adapter/handlers/ethics_approved.py
@@ -0,0 +1,4 @@
+
+
+def parse_proposal_payload(payload):
+    pass
---
+++ rmas_pm_adapter/settings.py
@@ -0,0 +1,8 @@
+
+
+
+RMAS_BUS_WSDL='http://localhost:7789/?wsdl'
+POLL_INTERVAL=5000
+EVENTS=[
+        ('ethics-approved', 'handlers.ethics_approved'),
+        ]
