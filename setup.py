6e96de96156621250157b2a37103968c31ecdaa7
Jason Marshall <ctrlspc@gmail.com>
2012-12-20 16:44:58 +0000

    updated requirements

--- setup.py
+++ setup.py
@@ -14,9 +14,7 @@ setup(
     description='A basic framework for building RMAS adapters',
     long_description=open('README.md').read(),
     install_requires=[
-        "pymongo == 2.3",
         "requests == 0.14.1",
         "RMASAdapter == 0.1.4 ",
-        "pika==0.9.8",
     ],
 )
